b06d4d171e09d3855e42949032a96a64804a96ec
KrystianKaminski <krystiankaminski07@gmail.com>
2019-10-21 16:07:56 +0200

    Added more Coin components & installed proptypes lib

--- src/components/CoinList/CoinList.js
+++ src/components/CoinList/CoinList.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import Coin from './Coin/Coin'
+import React from "react";
+import Coin from "./Coin/Coin";
 
-const CoinList = (props) => {
-    return (
-        <div>
-            CoinList
-            <Coin
-                name="Bitcoin"
-                acronym="BTC"
-                value={11000}
-                cap={188698026949}
-            />
-        </div>
-    )
-}
+const CoinList = props => {
+  return (
+    <div>
+      CoinList
+      <Coin name="Bitcoin" acronym="BTC" value={11000} cap={188698026949} />
+      <Coin name="Etherum" acronym="ETH" value={842} cap={82581287147} />
+      <Coin name="NEO" acronym="NEO" value={117} cap={7624760000} />
+      <Coin name="EOS" acronym="EOS" value={7} cap={5513844716} />
+    </div>
+  );
+};
 
-export default CoinList
+export default CoinList;
