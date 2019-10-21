7478301a01ae14016ebf63ffebbde337f7d1ae2f
KrystianKaminski <krystiankaminski07@gmail.com>
2019-10-21 14:23:46 +0200

    Initial structure of project

---
+++ src/components/CoinList/CoinList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Coin from './Coin/Coin'
+
+const CoinList = (props) => {
+    return (
+        <div>
+            CoinList
+            <Coin/>
+        </div>
+    )
+}
+
+export default CoinList
