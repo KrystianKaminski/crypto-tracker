8605f75ac2813b9f5e69a02fa1205cb107f088ec
KrystianKaminski <krystiankaminski07@gmail.com>
2019-10-21 15:53:50 +0200

    Placeholder for header and coin components

--- src/components/CoinList/Coin/Coin.js
+++ src/components/CoinList/Coin/Coin.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Coin = props => {
     return (
-        <div>Coin</div>
+        <div>
+            Bitcoin
+            <ul className="crypto">
+                <li className="crypto__acronym">Acronym: BTC</li>
+                <li className="crypto__value">Current value: 8.800 $</li>
+                <li className="crypto__cap">Market cap: 2,3 mld</li>
+            </ul>
+        </div>
     )
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const Header = (props) => {
-    return <div>Header</div>
+    return (
+        <div>
+            <h1>Cryptocurrency Market Capitalizations</h1>
+            <div className="info">
+                <div className="info__crypto">
+                    Cryptocurrencies: 1494 / Markets: 8195
+                </div>
+                <div className="info__crypto">
+                   Market Cap: $591 331 002 804 / 24h Vol: $22 764 514 250 / BTC Dominance: 34.3%
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
