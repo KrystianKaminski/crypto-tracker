05dbbd8438660027e10778d45ce605c006ab6d6b
KrystianKaminski <krystiankaminski07@gmail.com>
2019-10-24 14:59:23 +0200

    Styled rest of components with styled-components

--- src/components/CoinList/CoinListCell/CoinListCell.js
+++ src/components/CoinList/CoinListCell/CoinListCell.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const CoinListCell = ({ isLarge, isHeader, additionalStyling, children }) => {
-  const fieldBorderColor = '#dedede';
-  const fieldPadding = 10;
-  const largeFieldFlex = '1 0 25%';
-  const smallFieldFlex = '1 0 10%';
+const fieldPadding = 10;
+const largeFieldFlex = '1 0 25%';
+const smallFieldFlex = '1 0 10%';
 
-  const styles = {
-    fieldContainer: {
-      display: 'flex',
-      alignItems: 'center',
-      flex: isLarge ? largeFieldFlex : smallFieldFlex,
-      fontWeight: isHeader ? 700 : 300,
-      paddingTop: fieldPadding,
-      paddingBottom: fieldPadding,
-      borderBottom: `${isHeader ? 4 : 2}px solid ${fieldBorderColor}`,
-    },
-  };
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex: ${props => (props.isLarge ? largeFieldFlex : smallFieldFlex)};
+  font-weight: ${props => (props.isHeader ? 700 : 300)};
+  padding: ${fieldPadding} 0;
+  border-bottom: ${props => (props.isHeader ? 4 : 2)}px solid #dedede;
+`;
 
+const CoinListCell = ({ isLarge, isHeader, children }) => {
   return (
-    <div
-      style={{
-        ...styles.fieldContainer,
-        ...additionalStyling,
-      }}
-    >
+    <Container isLarge={isLarge} isHeader={isHeader}>
       {children}
-    </div>
+    </Container>
   );
 };
 
 CoinListCell.propTypes = {
   isLarge: PropTypes.bool,
-  hasBoldText: PropTypes.bool,
-  hasThinBorder: PropTypes.bool,
+  isHeader: PropTypes.bool,
   additionalStyling: PropTypes.object,
   children: PropTypes.element.isRequired,
 };
 
 CoinListCell.defaultProps = {
+  isLarge: false,
   additionalStyling: {},
 };
 
