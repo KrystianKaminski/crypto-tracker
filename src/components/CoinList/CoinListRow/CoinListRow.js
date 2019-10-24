9994f8b05a1391408e63a2bdda26da6b23794a9b
KrystianKaminski <krystiankaminski07@gmail.com>
2019-10-24 14:51:00 +0200

    CoinListRow styled with styled-components

--- src/components/CoinList/CoinListRow/CoinListRow.js
+++ src/components/CoinList/CoinListRow/CoinListRow.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const CoinListRow = ({ children }) => {
-  const containerPadding = '15%';
-  const styles = {
-    container: {
-      display: 'flex',
-      flexDirection: 'row',
-      paddingLeft: containerPadding,
-      paddingRight: containerPadding,
-    },
-  };
+import sharedStyles from '../../../shared/styles/styles';
 
-  return <div style={styles.container}>{children}</div>;
-};
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-left: ${sharedStyles.contentPadding};
+  padding-right: ${sharedStyles.contentPadding};
+
+  @media (max-width: 800px) {
+    padding-left: ${sharedStyles.smallContentPadding};
+    padding-right: ${sharedStyles.smallContentPadding};
+    font-size: 0.8em;
+  }
+`;
+
+const CoinListRow = ({ children }) => <Container>{children}</Container>;
 
 CoinListRow.propTypes = {
-  children: PropTypes.element,
+  additionalStyling: PropTypes.object,
+  children: PropTypes.node.isRequired,
 };
 
 CoinListRow.defaultProps = {
